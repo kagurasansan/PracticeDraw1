05592b41aa986f6c955fc99d33cc73c6c8ad29ef
mayinfeng <13671210359@163.com>
2019-04-15 15:48:27 +0800

    fix

--- app/src/main/java/com/hencoder/hencoderpracticedraw1/practice/Practice8DrawArcView.java
+++ app/src/main/java/com/hencoder/hencoderpracticedraw1/practice/Practice8DrawArcView.java
@@ -2,12 +2,16 @@ package com.hencoder.hencoderpracticedraw1.practice;
 
 import android.content.Context;
 import android.graphics.Canvas;
+import android.graphics.Paint;
+import android.graphics.RectF;
 import android.support.annotation.Nullable;
 import android.util.AttributeSet;
 import android.view.View;
 
 public class Practice8DrawArcView extends View {
 
+    private Paint paint = new Paint(Paint.ANTI_ALIAS_FLAG);
+    private RectF mRectF = new RectF(100,100,600,400);
     public Practice8DrawArcView(Context context) {
         super(context);
     }
@@ -25,5 +29,11 @@ public class Practice8DrawArcView extends View {
         super.onDraw(canvas);
 
 //        练习内容：使用 canvas.drawArc() 方法画弧形和扇形
+        paint.setStyle(Paint.Style.FILL);
+        canvas.drawArc(mRectF,10,140,false,paint);
+        paint.setStyle(Paint.Style.STROKE);
+        canvas.drawArc(mRectF,180,60,false,paint);
+        paint.setStyle(Paint.Style.FILL);
+        canvas.drawArc(mRectF,240,80,true,paint);
     }
 }
--- app/src/main/java/com/hencoder/hencoderpracticedraw1/practice/Practice9DrawPathView.java
+++ app/src/main/java/com/hencoder/hencoderpracticedraw1/practice/Practice9DrawPathView.java
@@ -2,11 +2,16 @@ package com.hencoder.hencoderpracticedraw1.practice;
 
 import android.content.Context;
 import android.graphics.Canvas;
+import android.graphics.Paint;
+import android.graphics.Path;
 import android.support.annotation.Nullable;
 import android.util.AttributeSet;
 import android.view.View;
 
 public class Practice9DrawPathView extends View {
+
+    private Paint paint = new Paint(Paint.ANTI_ALIAS_FLAG);
+    private Path path = new Path();
 
     public Practice9DrawPathView(Context context) {
         super(context);
@@ -20,10 +25,16 @@ public class Practice9DrawPathView extends View {
         super(context, attrs, defStyleAttr);
     }
 
+    {
+
+    }
+
     @Override
     protected void onDraw(Canvas canvas) {
         super.onDraw(canvas);
-
-//        练习内容：使用 canvas.drawPath() 方法画心形
+        path.addArc(200, 100, 400, 300, -225, 225);
+        path.arcTo(400, 100, 600, 300, -180, 225, false);
+        path.lineTo(400, 420);
+        canvas.drawPath(path, paint);
     }
 }
